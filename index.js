b85350a5893b8ffcb07a606607b774328c0e2858
Irena <irena.podgurski@gmail.com>
2020-12-04 00:00:58 +0000

    fetch flyover times for ISS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // const { fetchMyIP } = require("./iss");
 // const { fetchCoordsByIP } = require("./iss");
+const { fetchISSFlyOverTimes } = require("./iss");
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -17,3 +18,10 @@
 //   console.log(coords);
 // });
 
+fetchISSFlyOverTimes({ latitude: "45.2722", longitude: "-75.7522" }, (error, flyoverTimes) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(flyoverTimes);
+});
